75dbebfee93a3a33c5d6183c093c1149de90ccf5
thbachmann86 <thbachmann86@9c06ab8c-4fb4-11de-9cb9-2f00d435f02c>
2009-10-09 14:02:57 +0000

    Fixes Issue 4 Fixes Issue 5 Fixes Issue 7 Fixes Issue 8

--- trunk/cocktailberater.de/application/modules/website/controllers/IndexController.php
+++ trunk/cocktailberater.de/application/modules/website/controllers/IndexController.php
@@ -248,12 +248,30 @@ class Website_IndexController extends Zend_Controller_Action {
 			}
 			// Kommentieren
 			if ($this->_hasParam ( 'comment' )) {
-				$comment = new Website_Model_Comment ( ) ;
-				$comment->recipeId = $this->_getParam ( 'id' );
-				$comment->memberId = $this->_getParam ( 'member' );
-				$comment->comment = $this->_getParam ( 'comment' );
-				$comment->ip = $_SERVER [ 'REMOTE_ADDR' ];
-				$comment->save();
+				$recaptcha = new Zend_Service_ReCaptcha('6LdKvggAAAAAAAhu1rSAwtIm1Ejzls4LDi0K27Td', '6LdKvggAAAAAADoKDLXugjbiSxuTc7zdrAVm8-qC');
+				try{
+					$result = $recaptcha->verify(
+					    $this->_getParam ( 'recaptcha_challenge_field' ),
+					    $this->_getParam ( 'recaptcha_response_field' )
+					);
+					if ($result->isValid()) {
+						$comment = new Website_Model_Comment ( ) ;
+						$comment->recipeId = mysql_escape_string(strip_tags($this->_getParam ( 'id' )));
+						//$comment->memberId = $this->_getParam ( 'member' );
+						$comment->memberId = NULL;
+						$comment->comment = mysql_escape_string(strip_tags($this->_getParam ( 'comment' )));
+						$comment->ip = mysql_escape_string(strip_tags($_SERVER [ 'REMOTE_ADDR' ]));
+						//Zend_Debug::dump($comment);
+						//exit;
+						$comment->save();
+						$this->view->success = 'Vielen Dank für deinen Kommentar!';
+					} else {
+						$this->view->error = 'Leider ist ein Fehler beim Einfügen deines Kommentares aufgetreten. Bitte überprüfe deine Eingabe!';
+					}
+				} catch (Zend_Exception $e){
+					$this->view->error = 'Leider ist ein Fehler beim Einfügen deines Kommentares aufgetreten. Bitte überprüfe deine Eingabe!';
+				}
+				
 			}
 			$recipe = new Website_Model_Recipe ( $this->_getParam ( 'id' ) ) ;
 			$this->view->recipe = $recipe;
