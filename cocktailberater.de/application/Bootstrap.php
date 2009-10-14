0fe0ea5cacbd4fa36af74edee3ca0ccabf3508e7
thbachmann86 <thobach@users.noreply.github.com>
2009-10-14 15:37:35 +0000

    redirect from default module to the website module

--- cocktailberater.de/application/Bootstrap.php
+++ cocktailberater.de/application/Bootstrap.php
@@ -1,6 +1,7 @@
 <?php
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
+
 	/**
 	 * Initialize Cache
 	 *
--- cocktailberater.de/application/modules/default/controllers/IndexController.php
+++ cocktailberater.de/application/modules/default/controllers/IndexController.php
@@ -8,11 +8,14 @@
 class IndexController extends Zend_Controller_Action{
 	
 	/**
+	 * forward to website module with it's layout
 	 * 
 	 * @return void
 	 */
 	public function indexAction() {
-
+		$this->_helper->layout()->setLayoutPath(APPLICATION_PATH.'/modules/website/layouts/');
+		$this->_forward('index','index','website');
 	}
+	
 }
 
