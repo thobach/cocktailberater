268d13d7231c96d609bad3bddb071e9db63f7380
thbachmann86 <thobach@users.noreply.github.com>
2009-10-14 14:35:58 +0000

    added xml sitemap

--- cocktailberater.de/application/modules/website/controllers/IndexController.php
+++ cocktailberater.de/application/modules/website/controllers/IndexController.php
@@ -5,7 +5,7 @@
  */
 
 class Website_IndexController extends Zend_Controller_Action {
-
+	
 	public function indexAction () {
 		$this->view->start = true ;
 	}
@@ -438,6 +438,81 @@ class Website_IndexController extends Zend_Controller_Action {
 		$form = new Website_Form_Contact();
 		$form->setAction($this->_helper->url('contact'));
 		return $form;
+	}
+	
+	/**
+	 * Returns a Sitemap XML File for search engines
+	 * 
+	 * @return String XML Sitemap
+	 */
+	public function sitemapAction()
+	{
+		// deactivate layout
+		$this->_helper->layout->disableLayout();
+		
+		// load navigation nodes
+		$config = new Zend_Config_Ini(APPLICATION_PATH.'/modules/website/config/navigation.ini', 'nav');
+		$pages = new Zend_Navigation($config);
+		
+		// add alcoholic cocktails
+		$alcoholicCocktails = Website_Model_Cocktail::listCocktails ( NULL, NULL, 'alcoholic' ) ;
+		$alcoholics = array();
+		foreach($alcoholicCocktails as $alcoholicCocktail){
+			foreach ($alcoholicCocktail->getRecipes() as $recipe){
+				$options = array();
+				$options['label'] = $recipe->name;
+				$options['action'] = 'recipe';
+				$options['module'] = 'website';
+				$options['params'] = array();
+				$options['params']['cocktail'] = $recipe->name;
+				$options['params']['id'] = $recipe->id;
+				$alcoholics[$recipe->id] = new Zend_Navigation_Page_Mvc($options);
+			}
+		}
+		$alcoholic = $pages->findOneBy('action','alcoholic');
+		$alcoholic->addPages($alcoholics);
+		
+		// add non-alcoholic cocktails
+		$nonAlcoholicCocktails = Website_Model_Cocktail::listCocktails ( NULL, NULL, 'non-alcoholic' ) ;
+		$nonAlcoholics = array();
+		foreach($nonAlcoholicCocktails as $nonAlcoholicCocktail){
+			foreach ($nonAlcoholicCocktail->getRecipes() as $recipe){
+				$options = array();
+				$options['label'] = $recipe->name;
+				$options['action'] = 'recipe';
+				$options['module'] = 'website';
+				$options['params'] = array();
+				$options['params']['cocktail'] = $recipe->name;
+				$options['params']['id'] = $recipe->id;
+				$nonAlcoholics[$recipe->id] = new Zend_Navigation_Page_Mvc($options);
+			}
+		}
+		$nonAlcoholic = $pages->findOneBy('action','non-alcoholic');
+		$nonAlcoholic->addPages($nonAlcoholics);
+		
+		// add top10 cocktails
+		$top10Cocktails = Website_Model_Cocktail::listCocktails ( NULL, NULL, 'top10' ) ;
+		$top10s = array();
+		foreach($top10Cocktails as $top10Cocktail){
+			foreach ($top10Cocktail->getRecipes() as $recipe){
+				$options = array();
+				$options['label'] = $recipe->name;
+				$options['action'] = 'recipe';
+				$options['module'] = 'website';
+				$options['params'] = array();
+				$options['params']['cocktail'] = $recipe->name;
+				$options['params']['id'] = $recipe->id;
+				$top10s[$recipe->id] = new Zend_Navigation_Page_Mvc($options);
+			}
+		}
+		$top10 = $pages->findOneBy('action','top10');
+		$top10->addPages($top10s);
+		
+		// pass all page nodes to the view
+		$this->view->pages = $pages;
+		
+		// set header to XML 
+		$this->getResponse()->setHeader('Content-Type', 'text/xml');
 	}
 
 }
