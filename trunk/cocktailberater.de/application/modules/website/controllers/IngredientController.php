69747a4de79296bbe7bc882ec7974b681c262450
thbachmann86 <thbachmann86@9c06ab8c-4fb4-11de-9cb9-2f00d435f02c>
2010-04-17 17:55:46 +0000

    display recipes of same ingredient fixed

--- trunk/cocktailberater.de/application/modules/website/controllers/IngredientController.php
+++ trunk/cocktailberater.de/application/modules/website/controllers/IngredientController.php
@@ -12,8 +12,8 @@ class Website_IngredientController extends Wb_Controller_RestController {
 		if ($this->_hasParam ( 'id' )) {
 			$ingredient = new Website_Model_Ingredient ( $this->_getParam ( 'id' ) ) ;
 			$this->view->ingredient = $ingredient ;
-			$liste = Website_Model_Cocktail::listCocktails ( $this->_getParam ( 'id' ), 100, 'zutat' ) ;
-			$this->view->cocktails = $liste ;
+			$liste = Website_Model_Recipe::searchByIngredient($ingredient->name);
+			$this->view->recipes = $liste ;
 		}
 	}
 	
