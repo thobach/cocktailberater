f63ef93a442fed656b336ca751b770afc1a58591
thbachmann86 <thbachmann86@9c06ab8c-4fb4-11de-9cb9-2f00d435f02c>
2010-06-03 12:43:54 +0000

    ?display=mobile or ?display=normal to overrule useragent detection

--- trunk/cocktailberater.de/application/Wb/Controller/Plugin/Layout.php
+++ trunk/cocktailberater.de/application/Wb/Controller/Plugin/Layout.php
@@ -11,15 +11,26 @@ class Wb_Controller_Plugin_Layout extends Zend_Controller_Plugin_Abstract {
 		$layout = Zend_Layout::getMvcInstance();
 		$layout->setLayoutPath(APPLICATION_PATH . '/modules/' .
 		$request->getModuleName() . '/layouts');
+		// skip if normal layout is wanted explicitly
+		if($request->getUserParam('display') == 'normal'){
+			return;
+		}
+		// check if mobile layout is wanted explicitly
+		if($request->getUserParam('display') == 'mobile'){
+			// set mobile layout
+			Zend_Layout::getMvcInstance()->setLayout('mobilelayout');
+			return;
+		}
 		// get user agent
 		$uAgent = $request->HTTP_USER_AGENT;
-		// check if useragent contains 'mobile' or 'webos' 
+		// check if useragent contains 'mobile' or 'webos'
 		foreach ($this->_agents as $agent => $negation) {
 			if (stripos($uAgent, $agent) !== false) {
+				// but not on mobile -> ipad
 				if ($negation) {
 					foreach ($negation as $neg) {
 						// check if anything is excluded from 'mobile' or 'webos'
-						// like a special page for iphone/ipad
+						// like a normal page for ipad
 						if (stripos($uAgent, $neg) !== false) {
 							return;
 						}
