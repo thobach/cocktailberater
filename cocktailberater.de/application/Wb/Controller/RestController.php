1e2fd19f5ad3f8ec61ef6058fdd890dd3a7a1629
thbachmann86 <thobach@users.noreply.github.com>
2010-06-03 15:28:16 +0000

    search form for mobile

--- cocktailberater.de/application/Wb/Controller/RestController.php
+++ cocktailberater.de/application/Wb/Controller/RestController.php
@@ -13,34 +13,47 @@ abstract class Wb_Controller_RestController extends Zend_Rest_Controller {
 
 		$contextSwitch = Zend_Controller_Action_HelperBroker::getStaticHelper('ContextSwitch');
 		$contextSwitch->setAutoJsonSerialization(false);
-		$contextSwitch->addContext('rss',array(
+		if(!$contextSwitch->hasContext('rss')){
+			$contextSwitch->addContext('rss',array(
 				'suffix'	=> 'rss',
 				'headers'	=> array('Content-Type' => 'application/rss+xml')));
-		$contextSwitch->addContext('atom',array(
+		}
+		if(!$contextSwitch->hasContext('atom')){
+			$contextSwitch->addContext('atom',array(
 				'suffix'	=> 'atom',
 				'headers'	=> array('Content-Type' => 'application/atom+xml')));
-		$contextSwitch->addContext('pdf',array(
+		}
+		if(!$contextSwitch->hasContext('pdf')){
+			$contextSwitch->addContext('pdf',array(
 				'suffix'	=> 'pdf',
 				'headers'	=> array('Content-Type' => 'application/pdf')));
-		$contextSwitch->addContext('rdf',array(
+		}
+		if(!$contextSwitch->hasContext('rdf')){
+			$contextSwitch->addContext('rdf',array(
 				'suffix'	=> 'rdf',
 				'headers'	=> array('Content-Type' => 'application/rdf+xml')));
-		$contextSwitch->addContext('ajax',array(
+		}
+		if(!$contextSwitch->hasContext('ajax')){
+			$contextSwitch->addContext('ajax',array(
 				'suffix'	=> 'ajax',
 				'headers'	=> array('Content-Type' => 'text/html')));
-		$contextSwitch->addContext('mobile',array(
+		}
+		if(!$contextSwitch->hasContext('mobile')){
+			$contextSwitch->addContext('mobile',array(
 				'suffix'	=> 'mobile',
 				'headers'	=> array('Content-Type' => 'text/html'),
 				'callbacks' => array(
 					'init'	=> array(__CLASS__, 'enableLayout'),
 		            'post' => array(__CLASS__, 'setLayoutContext'))));
-		$contextSwitch->addContext('html',array(
+		}
+		if(!$contextSwitch->hasContext('html')){
+			$contextSwitch->addContext('html',array(
 				'suffix'	=> '',
 				'headers'	=> array('Content-Type' => 'text/html'),
 				'callbacks' => array(
 					'init'	=> array(__CLASS__, 'enableLayout'),
 		            'post' => array(__CLASS__, 'setLayoutContext'))));
-
+		}
 
 		$contextSwitch = $this->_helper->getHelper('contextSwitch');
 		$contextSwitch->addActionContext('index', true);
@@ -102,7 +115,9 @@ abstract class Wb_Controller_RestController extends Zend_Rest_Controller {
 		if (null !== $layout && $layout->isEnabled()) {
 			$context = Zend_Controller_Action_HelperBroker::getStaticHelper('ContextSwitch')->getCurrentContext();
 			if (null !== $context) {
-				$layout->setLayout($layout->getLayout() . '.' . $context);
+				if(stripos($layout->getLayout(),$context) === false){
+					$layout->setLayout($layout->getLayout() . '.' . $context);
+				}
 			}
 		}
 	}
