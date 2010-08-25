d2532a9ed3d4e818255765f19f15d41f6075c181
thobach@web.de <thobach@web.de@9c06ab8c-4fb4-11de-9cb9-2f00d435f02c>
2010-08-25 14:20:28 +0000

    ACCEPTED - task 26: url 404 http://code.google.com/p/cocktailberater/issues/detail?id=26

--- trunk/cocktailberater.de/application/modules/default/controllers/IndexController.php
+++ trunk/cocktailberater.de/application/modules/default/controllers/IndexController.php
@@ -15,6 +15,15 @@ class IndexController extends Zend_Controller_Action{
 	public function indexAction() {
 		$this->_forward('index','index','website');	
 	}
+	
+	// @todo: remove in December 2010
+	public function recipeAction(){
+		$this->_redirect($this->view->url(array(
+							'module'=>'website',
+							'controller'=>'recipe',
+							'action'=>'get',
+							'id'=>$this->_getParam('id')),'rest',true));
+	}
 
 }
 
