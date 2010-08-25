87b3daeacf42efa35b673b19e65e5bac27e57c57
thobach@web.de <thobach@users.noreply.github.com>
2010-08-25 14:16:52 +0000

    FIXED - task 44: redirect /website bad für SEO http://code.google.com/p/cocktailberater/issues/detail?id=44

--- cocktailberater.de/application/modules/default/controllers/IndexController.php
+++ cocktailberater.de/application/modules/default/controllers/IndexController.php
@@ -8,21 +8,12 @@
 class IndexController extends Zend_Controller_Action{
 
 	/**
-	 * forward to website module with it's layout
+	 * display website module with it's layout
 	 *
 	 * @return void
 	 */
 	public function indexAction() {
-		//$this->_helper->layout()->setLayoutPath(APPLICATION_PATH.'/modules/website/layouts/');
-		$this->_redirect($this->view->url(array(
-							'module'=>'website',
-							'controller'=>'index',
-							'action'=>'index'),null,true).'?format='.$this->_getParam('format','html'));
-		/* 
-		 * a simple forward made the url() helper work wrong (didn't accept 
-		 * the actions)
-		 * $this->_forward('index','index','website');
-		 */ 
+		$this->_forward('index','index','website');	
 	}
 
 }
