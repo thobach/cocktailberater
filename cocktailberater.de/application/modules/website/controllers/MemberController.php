ecd94329a42b9b5ecedab14b8823252bd32dcfdf
thobach@web.de <thobach@users.noreply.github.com>
2010-10-24 16:47:02 +0000

    refactoring -> authenticate is now login, more comments

--- cocktailberater.de/application/modules/website/controllers/MemberController.php
+++ cocktailberater.de/application/modules/website/controllers/MemberController.php
@@ -8,6 +8,12 @@
 require_once(APPLICATION_PATH.'/Wb/Controller/RestController.php');
 class Website_MemberController extends Wb_Controller_RestController {
 
+	/**
+	 * No List of Members implemented
+	 * 
+	 * (non-PHPdoc)
+	 * @see application/Wb/Controller/Wb_Controller_RestController::indexAction()
+	 */
 	public function indexAction() {
 		$log = Zend_Registry::get('logger');
 		$log->log('Website_MemberController->indexAction',Zend_Log::DEBUG);
@@ -15,20 +21,26 @@ class Website_MemberController extends Wb_Controller_RestController {
 		throw new Zend_Controller_Action_Exception('Seite wurde nicht gefunden.', 404);
 	}
 
+	/**
+	 * Create new Member
+	 * 
+	 * (non-PHPdoc)
+	 * @see application/Wb/Controller/Wb_Controller_RestController::postAction()
+	 */
 	public function postAction() {
 		$log = Zend_Registry::get('logger');
 		$log->log('Website_MemberController->postAction',Zend_Log::DEBUG);
 
-		if(!$this->_hasParam('email')){
+		if(!$this->_hasParam('email') || $this->_getParam('email') == ''){
 			throw new Website_Model_MemberException('Email missing!');
 		}
-		if(!$this->_hasParam('password')){
+		if(!$this->_hasParam('password') || $this->_getParam('password') == ''){
 			throw new Website_Model_MemberException('Password missing!');
 		}
-		if(!$this->_hasParam('firstname')){
+		if(!$this->_hasParam('firstname') || $this->_getParam('firstname') == ''){
 			throw new Website_Model_MemberException('First name missing!');
 		}
-		if(!$this->_hasParam('lastname')){
+		if(!$this->_hasParam('lastname') || $this->_getParam('lastname') == ''){
 			throw new Website_Model_MemberException('Last name missing!');
 		}
 		if(Website_Model_Member::existsByEmail($this->_getParam('email'))){
@@ -48,6 +60,12 @@ class Website_MemberController extends Wb_Controller_RestController {
 		$this->_forward('get','member','website',array('id'=>$member->id));
 	}
 
+	/**
+	 * Deletes a Member by given e-mail address
+	 * 
+	 * (non-PHPdoc)
+	 * @see application/Wb/Controller/Wb_Controller_RestController::deleteAction()
+	 */
 	public function deleteAction() {
 		$log = Zend_Registry::get('logger');
 		$log->log('Website_MemberController->deleteAction',Zend_Log::DEBUG);
@@ -63,7 +81,7 @@ class Website_MemberController extends Wb_Controller_RestController {
 			throw new Website_Model_MemberException('User does not exist!');
 		}
 
-		if($member->authenticate($this->_getParam('password'))){
+		if($member->login($this->_getParam('password'))){
 			$member->delete();
 			$log->log('member deleted',Zend_Log::DEBUG);
 			$this->view->status='ok';
@@ -74,6 +92,12 @@ class Website_MemberController extends Wb_Controller_RestController {
 		}
 	}
 
+	/**
+	 * Returns information on single member, given by id
+	 * 
+	 * (non-PHPdoc)
+	 * @see application/Wb/Controller/Wb_Controller_RestController::getAction()
+	 */
 	public function getAction() {
 		$log = Zend_Registry::get('logger');
 		$log->log('Website_MemberController->getAction',Zend_Log::DEBUG);
