8404ffb911972a78183b9b69688480b3b3009636
thobach@web.de <thobach@users.noreply.github.com>
2010-10-24 16:43:38 +0000

    added post (create) for bars

--- cocktailberater.de/application/modules/website/controllers/BarController.php
+++ cocktailberater.de/application/modules/website/controllers/BarController.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Context sensitive Controller for cocktail matters
+ * Context sensitive Controller for bar matters
  *
  * @author thobach
  *
@@ -14,4 +14,38 @@ class Website_BarController extends Wb_Controller_RestController {
 		$this->view->title = 'Barlist';
 	}
 
+	/**
+	 * Create new Bar
+	 * 
+	 * (non-PHPdoc)
+	 * @see application/Wb/Controller/Wb_Controller_RestController::postAction()
+	 */
+	public function postAction() {
+		$log = Zend_Registry::get('logger');
+		$log->log('Website_BarController->postAction',Zend_Log::DEBUG);
+
+		if(!$this->_hasParam('owner') || $this->_getParam('owner') == ''){
+			throw new Website_Model_MemberException('Owner missing!');
+		}
+		if(!$this->_hasParam('name') || $this->_getParam('name') == ''){
+			throw new Website_Model_MemberException('Name missing!');
+		}
+		if(!$this->_hasParam('location') || $this->_getParam('location') == ''){
+			throw new Website_Model_MemberException('Location missing!');
+		}
+
+		$bar = new Website_Model_Bar();
+		$bar->ownerId = $this->_getParam('owner');
+		$bar->name = $this->_getParam('name');
+		$bar->location = $this->_getParam('location');
+		// default for now is germany
+		$bar->country = 'de';
+		$bar->save();
+
+		// created
+		$this->getResponse()->setHttpResponseCode(201);
+
+		$this->_forward('get','bar','website',array('id'=>$bar->id));
+	}
+	
 }
